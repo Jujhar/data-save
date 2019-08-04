03a06d98583b6bbeb5ab6729ce5bcd7a126e3038
Jujhar <jujhar.pannu@gmail.com>
2019-08-04 15:34:59 -0700

    Add program logic + add readme

---
+++ readme.md
@@ -0,0 +1,10 @@
+Read and write data on a simple Rust HTTP Api
+
+## Run
+
+`cargo run`
+
+## Endpoints
+
+ * /save/:name/:value
+ * /get/:name
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@
 // notice may not be copied, modified, or distributed except
 // according to those terms.
 
+use std::fs;
+
 #[macro_use]
 extern crate rouille;
 
@@ -17,17 +19,32 @@ fn main() {
 
         router!(request,
             (GET) (/) => {
-                rouille::Response::text("Endpoints \n\n /save/:name/:value \n /get/:name/")
+                rouille::Response::text("Endpoints \n\n /save/:name/:value \n /load/:name/")
+
+                // To do list items here
             },
 
+            // Create a new file with contents as value param and filename as name param
             (GET) (/save/{name: String}/{value: String}) => {
-                println!("will save {}", name);
-                rouille::Response::text("save")
+                let output = value.to_string();
+                print!("Save {}", name);
+                let checkmark = '\u{2713}';
+                println!(" {}", checkmark);
+                fs::write(format!("save/{}", name), value).expect("Unable to write file");
+                rouille::Response::text(format!("saved {}", output))
             },
 
+            // Retrieve file based on filename and display data
+            (GET) (/load/{name: String}) => {
+                print!("Get {}", name);
+                let data = fs::read_to_string(format!("save/{}", name)).expect("Unable to read file");
+                println!(" --> Got {}", data);
+                rouille::Response::text(data)
+            },
+
+            // Redirect in case someone accidentally uses /get instead of /load
             (GET) (/get/{name: String}) => {
-                println!("will get {}", name);
-                rouille::Response::text("get")
+                rouille::Response::redirect_302(format!("/load/{}", name))
             },
 
             (GET) (/panic) => {
